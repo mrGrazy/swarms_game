b649370c01aab071e1740057ed7de387385995dc
Andy Gray <mr.andy.gray@gmail.com>
2013-04-20 19:59:49 +1200

    gave the hunter a facing, instead of velocity

--- hunter.py
+++ hunter.py
@@ -5,15 +5,20 @@ class Hunter:
   
   def __init__(self):
     self.pos = Vec2d(0,0)
-    self.velo = Vec2d(0.1,0.1)
+    self.facing = 1
+    self.speed = 0.1
 
 
   def update(self, delta_time):
+    velo = Vec2d(1,1)
+    velo.length = self.speed
+    velo.angle = self.facing
     self.pos = self.pos + (self.velo * delta_time)
     pass
 
   def draw(self, surface):
     surface.translate(self.pos.x, self.pos.y)
+    surface.rotate(self.velo.get_angle())
     surface.rectangle(-5,-5,10,10)
     surface.set_source_rgb(0, 0, 0)
     surface.stroke()
