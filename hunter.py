944e664cc9e1c18a6a15dd684f84936386abe658
Andy Gray <mr.andy.gray@gmail.com>
2013-04-21 14:15:29 +1200

    Added fps and lots of hunters with various rotational velocities

--- hunter.py
+++ hunter.py
@@ -1,26 +1,26 @@
 import pygame.draw
 from vector2d import Vec2d
 import math
+import random
 
 class Hunter:
   
   def __init__(self):
-    self.pos = Vec2d(0,0)
+    self.pos = Vec2d(random.randint(-200,200),random.randint(-200,200))
     self.facing = 0
-    self.speed = 30
-
+    self.speed = 50
+    self.rot_speed = random.uniform(-1,1)
 
   def update(self, delta_time):
-    self.facing += 0.2*delta_time
-    self.velo = Vec2d(1,1)
-    self.velo.length = self.speed * delta_time
+    self.facing += self.rot_speed*delta_time
+    self.velo = Vec2d(0,self.speed * delta_time)
     self.velo.angle = self.facing
     self.pos = self.pos + self.velo
-    pass
 
   def draw(self, surface):
     surface.translate(self.pos.x, self.pos.y)
     surface.rotate(self.facing)
+    surface.set_line_width(1.5)
     surface.move_to(10,0)
     surface.line_to(-10,5)
     surface.line_to(-10,-5)
--- world.py
+++ world.py
@@ -6,8 +6,9 @@ class World:
   def __init__(self):
     self.objects = []
     #self.objects.append(Player())
-    self.objects.append(Hunter())
-
+    for i in range(100):
+      self.objects.append(Hunter())
+    
   def update(self, delta_time):
     for obj in self.objects:
       obj.update(delta_time)
